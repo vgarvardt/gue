77861e6774e5a48ca5b0c5e22b5caf90f4b575b3
Ivan Trubach <mr.trubach@icloud.com>
2021-06-04 11:39:22 +0300

    Added adapter for github.com/go-pg/pg/v10

---
+++ adapter/testing/gopgv10.go
@@ -0,0 +1,44 @@
+package testing
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/stretchr/testify/require"
+
+	"github.com/vgarvardt/gue/v2/adapter"
+	"github.com/vgarvardt/gue/v2/adapter/gopgv10"
+)
+
+// OpenTestPoolMaxConnsGoPGv10 opens connections pool used in testing
+func OpenTestPoolMaxConnsGoPGv10(t testing.TB, maxConnections int32) adapter.ConnPool {
+	t.Helper()
+
+	applyMigrations("").Do(func() {
+		doApplyMigrations(t, "")
+	})
+
+	opts, err := pg.ParseURL(testConnDSN(t))
+	require.NoError(t, err)
+
+	opts.PoolSize = int(maxConnections)
+
+	db := pg.Connect(opts)
+	require.NoError(t, db.Ping(context.Background()))
+
+	pool := gopgv10.NewConnPool(db)
+
+	t.Cleanup(func() {
+		truncateAndClose(t, pool)
+	})
+
+	return pool
+}
+
+// OpenTestPoolGoPGv10 opens connections pool used in testing
+func OpenTestPoolGoPGv10(t testing.TB) adapter.ConnPool {
+	t.Helper()
+
+	return OpenTestPoolMaxConnsPGXv4(t, defaultPoolConns)
+}
