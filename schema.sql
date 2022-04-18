681682909fea0e0bfce0b2f22f5848aae48d3a82
Vladimir Garvardt <vgarvardt@gmail.com>
2022-04-18 12:32:01 +0200

    Tuned linter rules and fixed existing issues

--- schema.sql
+++ schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS gue_jobs
 (
-    job_id      bigserial   NOT NULL PRIMARY KEY,
-    priority    smallint    NOT NULL,
-    run_at      timestamptz NOT NULL,
-    job_type    text        NOT NULL,
-    args        json        NOT NULL,
-    error_count integer     NOT NULL DEFAULT 0,
-    last_error  text,
-    queue       text        NOT NULL,
-    created_at  timestamptz NOT NULL,
-    updated_at  timestamptz NOT NULL
+    job_id      BIGSERIAL   NOT NULL PRIMARY KEY,
+    priority    SMALLINT    NOT NULL,
+    run_at      TIMESTAMPTZ NOT NULL,
+    job_type    TEXT        NOT NULL,
+    args        JSON        NOT NULL,
+    error_count INTEGER     NOT NULL DEFAULT 0,
+    last_error  TEXT,
+    queue       TEXT        NOT NULL,
+    created_at  TIMESTAMPTZ NOT NULL,
+    updated_at  TIMESTAMPTZ NOT NULL
 );
 
 CREATE INDEX IF NOT EXISTS "idx_gue_jobs_selector" ON "gue_jobs" ("queue", "run_at", "priority");
