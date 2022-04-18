7540e096a7c2e8c817467bc3ccb7cd5477690250
Vladimir Garvardt <vgarvardt@gmail.com>
2022-04-18 10:24:55 +0200

    Bumped test postgres to 10 as 9.6 reached EOL

--- adapter/testing/all.go
+++ adapter/testing/all.go
@@ -1,10 +1,20 @@
 package testing
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/vgarvardt/gue/v3/adapter"
 )
+
+const defaultPoolConns = 5
+
+var migrations sync.Map
+
+func applyMigrations(schema string) *sync.Once {
+	once, _ := migrations.LoadOrStore(schema, &sync.Once{})
+	return once.(*sync.Once)
+}
 
 // OpenTestPool callback type for opening connection pool with default parameters used in tests
 type OpenTestPool func(t testing.TB) adapter.ConnPool
