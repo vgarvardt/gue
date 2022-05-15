fa327f4d7a10d93fae4dc67ea2d55ca11cac0854
Vladimir Garvardt <vgarvardt@gmail.com>
2022-05-15 13:33:31 +0200

    Tune adapters coverage

---
+++ adapter/pgxv5/db_test.go
@@ -0,0 +1,12 @@
+package pgxv5
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestFake is the fake test to add the sub-package to coverage report as adapters are being tested by other packages.
+func TestFake(t *testing.T) {
+	assert.True(t, true)
+}
