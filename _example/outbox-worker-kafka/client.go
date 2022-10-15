5178c4e7c7364b5de583247a6cec4a5743a7deea
Vladimir Garvardt <vgarvardt@gmail.com>
2022-10-15 22:50:08 +0200

    Added example outbox worker kafka implementation

---
+++ _example/outbox-worker-kafka/client.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
+	"github.com/Shopify/sarama"
+	"github.com/google/uuid"
+	"github.com/spf13/cobra"
+	"github.com/vgarvardt/gue/v4"
+)
+
+func newClientCommand() *cobra.Command {
+	var gc *gue.Client
+
+	return &cobra.Command{
+		Use:   "client",
+		Short: "Outbox Worker Client, enqueues messages to the gue for further processing by teh worker",
+		PreRunE: func(cmd *cobra.Command, args []string) (err error) {
+			gc, err = newGueClient(cmd.Context())
+			return
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := cmd.Context()
+
+			quitCh := initQuitCh()
+			for {
+				select {
+				case sig := <-quitCh:
+					log.Printf("Received interrupt (%s), exiting app\n", sig.String())
+					return nil
+
+				case <-ctx.Done():
+					log.Printf("Received context done (err: %q), exiting app\n", ctx.Err().Error())
+					return nil
+
+				default:
+					var num int
+					if err := survey.AskOne(
+						&survey.Input{
+							Message: "How many message should I publish to kafka using outbox?",
+							Default: "10",
+						},
+						&num,
+						survey.WithValidator(survey.Required),
+					); err != nil {
+						if err == terminal.InterruptErr {
+							log.Printf("Received terminal interrupt, exiting app\n")
+							return nil
+						}
+						return fmt.Errorf("could not get get response on how many message to publish: %w", err)
+					}
+
+					if num < 1 {
+						log.Printf("Number of messages to publish must be greater than zero\n")
+						continue
+					}
+
+					now := time.Now()
+					for i := 0; i < num; i++ {
+						msg := outboxMessage{
+							Topic: kafkaTopic,
+							Key:   []byte(fmt.Sprintf("%s-%d", now.String(), i%2)),
+							Value: []byte(fmt.Sprintf("message #%d @ %s", i, now.String())),
+							Headers: []sarama.RecordHeader{
+								{Key: []byte("message-uuid"), Value: []byte(uuid.NewString())},
+							},
+						}
+
+						args, err := json.Marshal(msg)
+						if err != nil {
+							return fmt.Errorf("could not marshal message to json: %w", err)
+						}
+
+						if err := gc.Enqueue(ctx, &gue.Job{
+							Queue: outboxQueue,
+							Type:  outboxJobType,
+							Args:  args,
+						}); err != nil {
+							return fmt.Errorf("could not enqueue job: %w", err)
+						}
+
+						log.Printf(
+							"Enqueued message for publishing to the gue: topic %q, key %q, value %q\n",
+							msg.Topic, msg.Key, msg.Value,
+						)
+					}
+				}
+			}
+		},
+	}
+}
---
+++ _example/outbox-worker-kafka/worker.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/spf13/cobra"
+	"github.com/vgarvardt/gue/v4"
+	"github.com/vgarvardt/gue/v4/adapter"
+)
+
+func newWorkerCommand() *cobra.Command {
+	var (
+		gc       *gue.Client
+		producer sarama.SyncProducer
+	)
+
+	return &cobra.Command{
+		Use:   "worker",
+		Short: "Outbox Worker, reads gue messages enqueued by the client and publishes them to Kafka",
+		PreRunE: func(cmd *cobra.Command, args []string) (err error) {
+			gc, err = newGueClient(cmd.Context())
+			if err != nil {
+				return
+			}
+
+			if err = createTestTopic(); err != nil {
+				return
+			}
+
+			producer, err = newSyncProducer()
+			if err != nil {
+				return
+			}
+
+			return
+		},
+		PostRunE: func(cmd *cobra.Command, args []string) error {
+			return producer.Close()
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			wm := gue.WorkMap{
+				outboxJobType: outboxMessageHandler(producer),
+			}
+
+			worker, err := gue.NewWorker(
+				gc, wm,
+				gue.WithWorkerQueue(outboxQueue),
+				gue.WithWorkerLogger(adapter.NewStdLogger()),
+				gue.WithWorkerPollInterval(500*time.Millisecond),
+				gue.WithWorkerPollStrategy(gue.RunAtPollStrategy),
+				gue.WithWorkerID("outbox-worker-"+gue.RandomStringID()),
+			)
+			if err != nil {
+				return fmt.Errorf("could not build gue worker: %w", err)
+			}
+
+			cancelCtx, cancel := context.WithCancel(cmd.Context())
+			defer cancel()
+
+			go func() {
+				if err := worker.Run(cancelCtx); err != nil {
+					log.Fatalf("Worker finished with error: %s\n", err)
+				}
+				log.Println("Worker finished")
+			}()
+
+			quitCh := initQuitCh()
+			sig := <-quitCh
+			log.Printf("Received interrupt (%s), exiting app\n", sig.String())
+			cancel()
+			return nil
+		},
+	}
+}
+
+func outboxMessageHandler(producer sarama.SyncProducer) gue.WorkFunc {
+	return func(ctx context.Context, j *gue.Job) error {
+		var m outboxMessage
+		if err := json.Unmarshal(j.Args, &m); err != nil {
+			return fmt.Errorf("could not unmarshal kafka oubox message: %w", err)
+		}
+
+		pm := sarama.ProducerMessage{
+			Topic:   m.Topic,
+			Key:     sarama.ByteEncoder(m.Key),
+			Value:   sarama.ByteEncoder(m.Value),
+			Headers: m.Headers,
+		}
+		partition, offset, err := producer.SendMessage(&pm)
+		if err != nil {
+			return fmt.Errorf("could not publish message to kafka from outbox [job-id: %d]: %w", j.ID, err)
+		}
+
+		log.Printf(
+			"Published message to kafka: topic %q, partition %d, offset %d, key %q, value %q\n",
+			m.Topic, partition, offset, m.Key, m.Value,
+		)
+
+		return nil
+	}
+}
